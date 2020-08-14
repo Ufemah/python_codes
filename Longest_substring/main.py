c6882eb2264e8ec7c94bdcdd6c807b6c6a9fe64c
ufemah <Ufemah@gmail.com>
2020-08-14 13:31:49 +0300

    improved version

--- Longest_substring/main.py
+++ Longest_substring/main.py
@@ -9,26 +9,55 @@ class Solution:
         print(self.string)
 
     def length_of_substring(self):
-        pass
+        """
+        good solution
+        :return: length of longest substr w/o repeating
+        """
+        length = 0
+        res = ''
+        substr = ''
+
+        for el in self.string:
+
+            if el in res:
+                if len(res) > length:
+                    length = len(res)
+                    substr = res
+                res = res[res.index(el) - 1::][1::]
+            else:
+                res += el
+
+            if length > int(len(self.string) / 2) + 1:
+                return length
+
+        # print(substr)
+        return length
 
     def long_way(self):
         """
         easy and slow
         :return: length of longest substr w/o repeating
         """
-        res = 0
+        length = 0
+        substr = ''
 
         for i in range(len(self.string)):
             for j in range(len(self.string)):
                 if i < j:
-                    if len(self.string[i:j]) == len(set(self.string[i:j])) and len(self.string[i:j]) > res:
-                        res = len(self.string[i:j])
+                    el = self.string[i:j]
 
-        return res
+                    if len(el) == len(set(el)) and len(el) > length:
+                        length = len(el)
+                        substr = el
+
+                        if length > int(len(self.string) / 2) + 1:
+                            return length
+        # print(substr)
+        return length
 
 
 if __name__ == '__main__':
     inp = 'abrka' + 'abcdefghij' + 'jxxx'
     s = Solution(inp)
     print(s.length_of_substring())   # 10
-    print(s.long_way())
+    print(s.long_way())              # 10
