4935316f7ba0fd62aaa21d6932ffe4ebf1b7631a
Ufemah <ufemah@gmail.com>
2021-04-22 18:31:01 +0300

    look at this

---
+++ test.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import datetime
+
+root = tk.Tk()
+
+can = tk.Canvas(height=500, width=1000)
+can.pack()
+
+rect = can.create_rectangle(0, 240, 20, 260, fil='#5F6A6A')
+
+
+def act():
+    global rect, can
+    pos = can.coords(rect)
+    if pos[2] < 1000:
+        can.move(rect, 5, 0)
+        can.update()
+        can.after(1)
+        act()
+        
+
+def act2():
+    global rect, can
+    pos = can.coords(rect)
+    if pos[3] < 500:
+        can.move(rect, 0, 5)
+        can.update()
+        can.after(1)
+        act2()
+  
+
+def key_down(key):
+    act()
+    act2()
+    
+
+can.bind("<Button-1>", key_down)
+root.mainloop()
